ddd2bdd7b0efc0eb793a9ceddd44f5ab6abc1151
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-27 09:16:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["cat", "dog", "chicken", "cow"] << "pig"
+  array << "pig"
 end
 
 def add_element_to_start_of_array(array, element)
